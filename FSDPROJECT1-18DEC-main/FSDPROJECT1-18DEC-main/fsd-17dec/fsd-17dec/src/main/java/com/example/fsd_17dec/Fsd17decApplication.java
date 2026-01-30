d27cba381e0df058821956bb0939e50557ef08f6
bharath chowdary <cheredlabharath21@gmail.com>
2026-01-30 11:39:50 +0530

    Add files via upload

---
+++ FSDPROJECT1-18DEC-main/FSDPROJECT1-18DEC-main/fsd-17dec/fsd-17dec/src/main/java/com/example/fsd_17dec/Fsd17decApplication.java
@@ -0,0 +1,14 @@
+package com.example.fsd_17dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fsd17decApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fsd17decApplication.class, args);
+		System.out.println("Section 67");
+	}
+
+}
---
+++ FSDPROJECT1-18DEC-main/FSDPROJECT1-18DEC-main/fsd-17dec/fsd-17dec/src/test/java/com/example/fsd_17dec/Fsd17decApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fsd_17dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fsd17decApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
